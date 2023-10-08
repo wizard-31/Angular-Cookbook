6c1bbf20eb9bb3c6eb002bb1494ca358080f6036
Subramanian Venkataraman <subvnkt@gmail.com>
2023-10-08 16:24:22 -0400

    Setup Recipes and Ingredients Model and defined base skeleton

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipes.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit{
+  recipes: Recipe[] = [
+    new Recipe('Garden Salad', 'Healthy Salad', 'https://upload.wikimedia.org/wikipedia/commons/a/a9/10_minute_Recipe_for_a_Healthy_Garden_Salad_-_49859886512.jpg'),
+  ];
+
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+  }
 
 }
